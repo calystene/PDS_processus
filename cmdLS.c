cf29f3f612103e73f9630460055921b036e4e984
Calystene <pierard.t@gmail.com>
2014-02-06 14:57:04 +0100

    avancemeent TP 40%

--- cmdLS.c
+++ cmdLS.c
@@ -11,19 +11,28 @@ main
 
   switch(fork()) 
     {
+      /*
+       * Erreur dans la création du fils
+       */
     case -1:
       {
 	perror("Erreur création fils");
 	return -1;
       } 
 
+      /*
+       * Le fils éxécute la commande systeme ls avec l'argument passé en paramètre du programme
+       */
     case 0:
       {
 	printf("Le fils va procéder à l'éxécution de ls\n");
 	sleep(3);
-	exit(execl("/bin/ls",argv[1],NULL));
+	exit(execl("/bin/ls","ls",argv[1],NULL));
       }
-
+      
+      /*
+       * Le père suite au résultat du fils crée le fichier si il est absent, ou le supprime si il est présent
+       */
     default:
       {
 	wait(&result);
@@ -31,11 +40,11 @@ main
 	sleep(3);
 	if(!result)
 	  {
-	    execl("/bin/rm", argv[1], NULL);
+	    execl("/bin/rm","rm",argv[1],NULL);
 	  } 
 	else 
 	  {
-	    execl("/bin/touch", argv[1], NULL);
+	    execl("/bin/touch","touch",argv[1],NULL);
 	  }
 
 	return 1;
